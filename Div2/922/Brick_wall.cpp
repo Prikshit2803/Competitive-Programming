467a227552fe3ff9bacc834d09ff066f8ad4fc4c
Prikshit2803 <119496129+Prikshit2803@users.noreply.github.com>
2024-01-31 00:45:42 +0530

    Create Brick_wall.cpp

---
+++ Div2/922/Brick_wall.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+#define int long long
+
+ 
+ 
+
+ 
+ 
+ 
+void solve(){
+        int n,m;
+        cin>>n>>m;
+        cout<<n*(m/2)<<endl;
+}
+ 
+ 
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+ 
+ 
+ 
+    int t=1;
+    cin>>t;
+    while(t--){
+         solve();
+    }
+ 
+ 
+    return 0;
+}
