32cc551b3e6a4cb183da7352d348aa9f176702b4
Prikshit2803 <119496129+Prikshit2803@users.noreply.github.com>
2024-02-01 14:14:52 +0530

    Update C_XOR-distance.cpp

--- Div2/922/C_XOR-distance.cpp
+++ Div2/922/C_XOR-distance.cpp
@@ -1,4 +1,82 @@
+//1st condition :a>b
+//Binary propertu if kh bit set > sum of (0 to k-1th) bit set 
+//Intuition : Minimize a and maximize b , such that a>b and minimum difference
+//start from left and find first bit that is set in a but not in b (it exists as a>b), set flag true from here on every bit set in 1 but not in b
+//we take x value =0 as it makes a bit 0 and b bit = 1 so that difference is minimized
+//also set x bit 1 only if it is less than k/r as 1<=x<=r
 
+//link :https://www.youtube.com/watch?v=amDG9gnk-6k
+#include<iostream>
+using namespace std;
+#define int long long
+ 
+ 
+ 
+ 
+ 
+ int find(int a,int b,int k){
+    bool flag=false;
+    
+    int x=0;
+    
+    for(int i=63;i>=0;i--){
+        int abit = ((a>>i) & 1);
+        int bbit = ((b>>i) & 1);
+        
+        if(abit==1 && bbit==0){
+            if(!flag){
+                flag=true;
+            }
+            else{
+                int nx=x+(1ll<<i);
+                if(nx<=k)
+                x+=(1ll<<i);
+                else
+                x+=0;
+            }
+        }
+        
+    }
+    
+    a^=x;
+    b^=x;
+    
+    return abs(a-b);
+}
+ 
+ 
+void solve(){
+   
+   int a,b,k;
+   
+   cin>>a>>b>>k;
+   
+   if(a<b)
+   swap(a,b);
+   
+   int ans=find(a,b,k);
+   cout<<ans<<endl;
+}
+ 
+ 
+ 
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+ 
+ 
+ 
+ 
+    int t=1;
+    cin>>t;
+    while(t--){
+         solve();
+    }
+ 
+ 
+    return 0;
+}
 //https://www.youtube.com/watch?v=ptehfjBEOrU
 
 #include <bits/stdc++.h>
